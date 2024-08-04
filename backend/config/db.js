eb62d847a8f68b2efc0cdb45fba11f2b2aaa2bea
Ajish <ajish.r@infospica.com>
2024-08-04 19:51:06 +0530

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,11 +10,3 @@ const connectDB = async () => {
 }
 
 export default connectDB
-
-PORT = 8000
-DB_URL = mongodb://localhost:27017/taskManagement
-JWT_SECRET = IF55f&*mnv8b
-ACTIVATION_SECRET = ghusd%@*567njbSJB
-GMAIL = ajishraj5@gmail.com
-GPASS = yeuebnsdsvfcrjoh
-NODE_ENV = development
