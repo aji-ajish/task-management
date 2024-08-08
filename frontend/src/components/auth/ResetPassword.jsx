29a71f0668d9a75f233be5e3467aa5c48e605724
Ajish <ajish.r@infospica.com>
2024-08-08 14:29:17 +0530

    bug fix

--- frontend/src/components/auth/ResetPassword.jsx
+++ frontend/src/components/auth/ResetPassword.jsx
@@ -8,44 +8,42 @@ export default function ResetPassword() {
         navigate('/')
     }
     return (
-        <section className="w-full h-screen flex flex-col items-center justify-center px-4">
-            <div className="max-w-sm w-full dark:text-white">
-                <div className="text-center">
-                    <div className="mt-5 space-y-2">
-                        <h3 className="dark:text-white text-2xl font-bold sm:text-3xl">Reset Your Password</h3>
-                    </div>
+        <div className="max-w-sm w-full dark:text-white">
+            <div className="text-center">
+                <div className="mt-5 space-y-2">
+                    <h3 className="dark:text-white text-2xl font-bold sm:text-3xl">Reset Your Password</h3>
                 </div>
-                <form
-                    onSubmit={(e) => e.preventDefault()}
-                    className="mt-8 space-y-5"
-                >
-                    <div>
-                        <label className="font-medium">
-                            New Password
-                        </label>
-                        <input
-                            type="password"
-                            required
-                            className="w-full mt-2 px-3 py-2 dark:text-white bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                        />
-                    </div>
-                    <div>
-                        <label className="font-medium">
-                            Confirm Password
-                        </label>
-                        <input
-                            type="password"
-                            required
-                            className="w-full mt-2 px-3 py-2 dark:text-white bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                        />
-                    </div>
-
-                    <button onClick={submitHandler} className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150"
-                    >
-                        Reset
-                    </button>
-                </form>
             </div>
-        </section>
+            <form
+                onSubmit={(e) => e.preventDefault()}
+                className="mt-8 space-y-5"
+            >
+                <div>
+                    <label className="font-medium">
+                        New Password
+                    </label>
+                    <input
+                        type="password"
+                        required
+                        className="w-full mt-2 px-3 py-2 dark:text-white bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                    />
+                </div>
+                <div>
+                    <label className="font-medium">
+                        Confirm Password
+                    </label>
+                    <input
+                        type="password"
+                        required
+                        className="w-full mt-2 px-3 py-2 dark:text-white bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                    />
+                </div>
+
+                <button onClick={submitHandler} className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150"
+                >
+                    Reset
+                </button>
+            </form>
+        </div>
     )
 }
